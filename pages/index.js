86e2a20d284b8fb5fabdc712115bfe23a182b7ab
Marco Trevisani <marco.trevisani@abstract.it>
2021-04-02 22:58:50 +0200

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+import styled from 'styled-components';
+
+const StyledDiv = styled('div')`
+  background-color: red;
+`;
 
 export default function Home() {
   return (
@@ -19,7 +25,7 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
+        <StyledDiv className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -47,7 +53,7 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
+        </StyledDiv>
       </main>
 
       <footer className={styles.footer}>
@@ -61,5 +67,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
