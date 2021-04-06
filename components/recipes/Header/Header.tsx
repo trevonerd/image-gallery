8bd828e2556a441d14c86138c9ce83d808b39e89
Marco Trevisani <marco.trevisani@abstract.it>
2021-04-06 16:28:17 +0200

    feat(components): added Header + improved svg support

---
+++ components/recipes/Header/Header.tsx
@@ -0,0 +1,27 @@
+// import React, { useState, useContext } from 'react';
+import styled from 'styled-components';
+
+import { Typography } from '@/components/ingredients/Typography';
+
+import { ReactComponent as GitHub } from '@/assets/github.svg';
+
+const HeaderContainer = styled('div')`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <Typography as="h1" size="xl">
+                Image Gallery
+            </Typography>
+            <a href="">
+                <GitHub width="20" height="20" />
+            </a>
+        </HeaderContainer>
+    );
+};
+
+export default Header;
